e1184c9c228bc07d68a879f4a6b10383320d2869
na2hiro <a@81.la>
2022-01-30 00:39:55 +0900

    Fix lint

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent#description
 export function fixedEncodeURIComponent(str: string) {
-  return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
-    return '%' + c.charCodeAt(0).toString(16).toUpperCase();
+  return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
+    return "%" + c.charCodeAt(0).toString(16).toUpperCase();
   });
 }

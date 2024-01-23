e99e1519a8de57ff89fd38a16482d854f13b7a6e
hgeldenhuys <hgeldenhuys@gmail.com>
2024-01-23 14:31:57 -0500

    Added refreshToken to verify AS A trusted store I WANT a refreshToken SO THAT I can continue working by refreshing the accessToken beyond the 2hr expiry window

--- src/Twitter2Strategy.ts
+++ src/Twitter2Strategy.ts
@@ -179,7 +179,7 @@ export class Twitter2Strategy<User> extends Strategy<
     }
     const body = await response.json();
     debug("access token " + JSON.stringify(body));
-    const { expires_in, access_token, scope } = body;
+    const { expires_in, access_token, scope, refresh_token } = body;
 
     // Verify the user and return it, or redirect
     try {

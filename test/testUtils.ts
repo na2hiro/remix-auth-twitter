e4329f0fd9f49b8ff22a739d77c49b85e90b7e33
Anwar <55035440+anwaroodin@users.noreply.github.com>
2025-06-17 01:09:50 +0100

    Remove Jest configuration and related test utilities

--- test/testUtils.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from "@remix-run/node";
-
-export function isResponse(obj: unknown) {
-  return obj && typeof obj === "object" && "status" in obj && "url" in obj;
-}
-
-export function assertResponse(obj: unknown): asserts obj is Response {
-  if (!isResponse(obj)) throw new Error("Expected Response");
-}
